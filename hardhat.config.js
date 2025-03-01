4ec4c7268c701e906e90120f68b3efe11d137364
projamkiller <fletcher@jamkillerproductions.io>
2025-03-01 06:44:44 -0600

    Commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,51 +1,43 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@thirdweb-dev/hardhat-deploy");
-require("dotenv").config();
 
-const requiredEnvVars = ["RPC_URL", "PRIVATE_KEY"];
-requiredEnvVars.forEach((varName) => {
-  if (!process.env[varName]) {
-    throw new Error(`Missing ${varName} in .env file`);
-  }
-});
+const dotenv = require("dotenv");
+dotenv.config();
 
 module.exports = {
   solidity: {
-    version: "0.8.17",
+    version: "0.8.19",
     settings: {
       optimizer: {
         enabled: true,
         runs: 200,
       },
-      viaIR: true,
     },
   },
-  defaultNetwork: "hardhat",
   networks: {
-    hardhat: {},
-    optimism: {
-      url: process.env.RPC_URL,
-      accounts: [process.env.PRIVATE_KEY],
-      chainId: 10,
-      gasPrice: "auto",
+    hardhat: {
+      chainId: 1337,
     },
-    localhost: {
-      url: "http://127.0.0.1:8545",
+    ethereum: {
+      url:
+        process.env.ETHEREUM_RPC_URL ||
+        "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID",
+      accounts: process.env.PRIVATE_KEY
+        ? [process.env.PRIVATE_KEY]
+        : [],
+    },
+    polygon: {
+      url:
+        process.env.POLYGON_RPC_URL ||
+        "https://polygon-mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID",
+      accounts: process.env.PRIVATE_KEY
+        ? [process.env.PRIVATE_KEY]
+        : [],
     },
   },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts",
-    deploy: "./scripts",
-    deployments: "./deployments",
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS ? true : false,
-  },
-  typechain: {
-    outDir: "./typechain",
-    target: "ethers-v5",
+  thirdweb: {
+    deployer: {
+      default: process.env.WALLET_ADDRESS,
+    },
   },
 };
