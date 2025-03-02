5934c2b5897046a91626d33b9be1873e8e45e32f
projamkiller <fletcher@jamkillerproductions.io>
2025-03-01 19:44:01 -0600

    Commit

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,11 +1,11 @@
 /// <reference types="vite/client" />
 
 // Optional: If you want to explicitly type your environment variables, you can do:
-// declare interface ImportMetaEnv {
-//   readonly VITE_CLAIM_CONTRACT_ADDRESS: string;
-//   // Add other variables here...
-// }
+declare interface ImportMetaEnv {
+    readonly VITE_CLAIM_CONTRACT_ADDRESS: string;
+    // Add other variables here...
+}
 
-// declare interface ImportMeta {
-//   readonly env: ImportMetaEnv;
-// }
+declare interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
